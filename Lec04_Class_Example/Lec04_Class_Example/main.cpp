b830e427eff22a1ccb28dedbaffb4d10bafa891c
jonghyun5866 <hyun5866@jbnu.ac.kr>
2026-04-03 10:54:49 +0900

    0403

---
+++ Lec04_Class_Example/Lec04_Class_Example/main.cpp
@@ -0,0 +1,43 @@
+#include "Rectangle.h"
+#include "Pet.h"
+#include "Music.h"
+using namespace std;
+int main() {
+	// Rectangle 1 객체 생성
+	Rectangle rect1(3.4, 2.5);
+	cout << "Rectangle 1:" << endl;
+	// printf("Rectangle 1:\n");
+	cout << "Area: " << rect1.getArea() << endl;
+	//
+	cout << "Perimeter:" << rect1.getPerimeter() << endl;
+	cout << "Is Square ? " << boolalpha << rect1.isSquare() << endl;
+	printf("========================================\n");
+
+	// Pet class test
+	Pet myPet("Kong", 5, "Cat");
+	cout << "My pet's name is " << myPet.getName() << endl;
+	// Puppy class test
+	Puppy myPuppy("Lucky", 10, "Dog", "Puddle");
+	cout << "My puppy's breed is " << myPuppy.getBread() << endl;
+	printf("========================================\n");
+
+	// Streaming Service test
+	MusicStreamingService myService("Spotify");
+	//음악 Service에 추가하기
+	myService.addMusic("SWIM", "BTS", "ARIRANG", 2026);
+	myService.addMusic("RUDE", "Hearts2Hearts", "RUDE", 2026);
+	myService.addMusic("BANGBANG", "IVE", "REVIVE", 2026);
+	myService.addMusic("JUMP", "BlackPink", "Jump", 2025);
+	// title로 search
+	string music_title;
+	cout << "Enter the Music Title: ";
+	cin >> music_title;
+	//scanf_s("%s", &music_title);
+	Music* result = myService.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "Found: " << result->getTitle() << " by " << result->getArtist() << endl;
+	}
+	else {
+		cout << "Not Found" << endl;
+	}
+}
